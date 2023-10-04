fa630bd2cb0257c0301321069ae2b84da4edc3b7
dmt041104111003 <daomanhtung4102003@gmail.com>
2023-10-05 04:37:29 +0700

    fix home

--- src/food/food.js
+++ src/food/food.js
@@ -1,15 +1,52 @@
 const toggleButton = document.querySelector('.btn-toggle');
+const icon = toggleButton.querySelector('.bi');
+const toggleSidebarButton = document.querySelector('.btn-toggle-sidebar');
+const sidebar = document.querySelector('.sidebar');
 
-toggleButton.addEventListener('click', () => {
+
+// Hàm để thay đổi trạng thái và biểu tượng
+function toggleTheme() {
     const isDarkMode = document.body.classList.toggle('dark-theme');
-    localStorage.setItem('darkMode', isDarkMode);
+    icon.classList.toggle('bi-brightness-high-fill', isDarkMode);
+    icon.classList.toggle('bi-moon-stars-fill', !isDarkMode);
     toggleButton.querySelector('.spn2').textContent = isDarkMode ? 'LIGHT' : 'DARK';
+    localStorage.setItem('darkMode', isDarkMode);
+}
+
+// Lấy trạng thái từ localStorage hoặc mặc định là sáng
+const darkMode = localStorage.getItem('darkMode') === 'true';
+toggleTheme(darkMode);
+
+toggleButton.addEventListener('click', toggleTheme);
+
+// Hàm để ẩn thanh sidebar
+function hideSidebar() {
+    sidebar.classList.add('sidebar-hidden');
+}
+
+// Hàm để hiện thanh sidebar
+function showSidebar() {
+    sidebar.classList.remove('sidebar-hidden');
+}
+
+// Bắt sự kiện khi nút được nhấp
+toggleSidebarButton.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-hidden');
 });
 
-const darkMode = localStorage.getItem('darkMode') === 'true';
-document.body.classList.toggle('dark-theme', darkMode);
+// Bắt sự kiện khi kích thước màn hình thay đổi
+function handleResize() {
+    if (window.innerWidth <= 768) {
+        hideSidebar();
+    } else {
+        showSidebar();
+    }
+}
 
-const sidebar = document.querySelector('.sidebar');
 window.addEventListener('scroll', () => {
     sidebar.style.top = window.scrollY + 'px';
 });
+
+
+window.addEventListener('resize', handleResize);
+handleResize(); // Gọi nó một lần để cài đặt ban đầu
--- src/home/home.js
+++ src/home/home.js
@@ -4,6 +4,14 @@ toggleButton.addEventListener('click', () => {
     const isDarkMode = document.body.classList.toggle('dark-theme');
     localStorage.setItem('darkMode', isDarkMode);
     toggleButton.querySelector('.spn2').textContent = isDarkMode ? 'LIGHT' : 'DARK';
+    // Thay đổi biểu tượng và văn bản
+    if (!isDarkMode) {
+        toggleButton.querySelector('.bi').classList.remove('bi-brightness-high-fill');
+        toggleButton.querySelector('.bi').classList.add('bi-moon-stars-fill');
+    } else {
+        toggleButton.querySelector('.bi').classList.remove('bi-moon-stars-fill');
+        toggleButton.querySelector('.bi').classList.add('bi-brightness-high-fill');
+    }
 });
 
 const darkMode = localStorage.getItem('darkMode') === 'true';
@@ -13,3 +21,38 @@ const sidebar = document.querySelector('.sidebar');
 window.addEventListener('scroll', () => {
     sidebar.style.top = window.scrollY + 'px';
 });
+
+
+// code mới 
+const toggleSidebarButton = document.querySelector('.btn-toggle-sidebar');
+
+// Hàm để ẩn thanh sidebar
+function hideSidebar() {
+    sidebar.classList.add('sidebar-hidden');
+}
+
+// Hàm để hiện thanh sidebar
+function showSidebar() {
+    sidebar.classList.remove('sidebar-hidden');
+}
+
+// Bắt sự kiện khi nút được nhấp
+toggleSidebarButton.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-hidden');
+});
+
+// Bắt sự kiện khi kích thước màn hình thay đổi
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+        hideSidebar();
+    } else {
+        showSidebar();
+    }
+});
+
+// Đảm bảo rằng ban đầu thanh sidebar được ẩn khi màn hình nhỏ
+if (window.innerWidth <= 768) {
+    hideSidebar();
+} else {
+    showSidebar();
+}
